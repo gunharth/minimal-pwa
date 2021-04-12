29131eb3baadbde160f73c87130f7a3c2e4f740f
Gunharth Randolf <hello@gunharth.io>
2021-04-12 21:01:22 +0200

    add service worker

--- sw.js
+++ sw.js
@@ -1,23 +1,26 @@
+const version = "minimal-pwa-v1";
+
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open('FILL_IN_CACHE_NAME_v1')
-            .then(cache => cache.addAll([
-                'index.html',
-                'main.js',
-                'style.css',
-                'icon16.png',
-                'icon192.png',
-                'icon196.png',
-                'icon512.png',
-                'manifest.webmanifest'
-            ]))
+      caches
+        .open(version) // version 1
+        .then((cache) =>
+          cache.addAll([
+            "index.html",
+            "icon16.png",
+            "icon192.png",
+            "icon196.png",
+            "icon512.png",
+            "manifest.webmanifest",
+          ])
+        )
     );
 });
 
 self.addEventListener('fetch', function(event) {
     event.respondWith(
       caches
-        .open("FILL_IN_CACHE_NAME_v1")
+        .open(version)
         .then(function (cache) {
           return cache.match(event.request);
         })
